5db4d170044751fa5a3154d4cc9ca92de8757e4c
liumengchen@ibantang.com <fbsender@lmc.local>
2017-05-17 15:28:22 +0800

    add ichaopai & zhequan

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,22 @@ require 'seo_domain'
 class HomeController < ApplicationController
   @@compete_brands = nil
   def index
-    if !is_robot?
+    if request.host == "www.ichaopai.cc"
+      render "ichaopai"
+      return
+    end
+    if !is_robot? && request.host != "www.zhequan.cc"
       redirect_to "http://taobao.iquan.net", status: 302
       return
     end
     if @@compete_brands.nil? || Time.now.to_i % 600 == 0
       @@compete_brands = CompeteBrand.select(:title, :keywords, :description, :host).to_a
+    end
+    @compete_brands = @@compete_brands
+    @path = "/"
+    if request.host == "www.zhequan.cc"
+      render "zhequan"
+      return
     end
     if request.host.include?(".youhui.iquan.net")
       brand = CompeteBrand.where(host: request.host).take
@@ -16,7 +26,5 @@ class HomeController < ApplicationController
       @keywords = brand.keywords
       @description = brand.description
     end
-    @compete_brands = @@compete_brands
-    @path = "/"
   end
 end
