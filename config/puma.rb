5e582f6ff206897361a454725cda6542c6d9abc6
liumengchen@ibantang.com <fbsender@lmc.local>
2017-10-25 00:36:54 +0800

    update puma

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ port        ENV.fetch("PORT") { 3100 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-bind 'unix:///tmp/iquan_puma.sock'
+bind 'unix:///tmp/17430_puma.sock'
 pidfile '/tmp/puma.pid'
 stdout_redirect "/tmp/puma.stdout.log", "/tmp/puma.stderr.log"
 
