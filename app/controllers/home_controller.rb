4d35930e8396afdec22bc146ed2a42be2e1ca791
liumengchen@ibantang.com <fbsender@lmc.local>
2017-10-26 21:48:52 +0800

    update home list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -74,14 +74,14 @@ class HomeController < ApplicationController
   end
 
   def diyquan_home
-    url = "http://z.17gwx.com/web/index/index?client_id=zhekou_app_h5&client_secret=81f454ac98956541b195f2c7f9e53a06&page=0&pagesize=20"
+    url = "http://z.17gwx.com/web/coupon/live?client_id=zhekou_app_h5&client_secret=81f454ac98956541b195f2c7f9e53a06&page=0&pagesize=20"
     @coupons = JSON.parse(Net::HTTP.get(URI(url)))["data"]["coupon_list"]
     @links = Link.where(status: 1).to_a
     render "diyquan/home", layout: "layouts/diyquan"
   end
 
   def m_diyquan_home
-    url = "http://z.17gwx.com/web/index/index?client_id=zhekou_app_h5&client_secret=81f454ac98956541b195f2c7f9e53a06&page=0&pagesize=20"
+    url = "http://z.17gwx.com/web/coupon/live?client_id=zhekou_app_h5&client_secret=81f454ac98956541b195f2c7f9e53a06&page=0&pagesize=20"
     @coupons = JSON.parse(Net::HTTP.get(URI(url)))["data"]["coupon_list"]
     render "m_diyquan/home", layout: "layouts/m_diyquan"
   end
