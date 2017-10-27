53b9e4599cb0427e706b0eb0dd0171967c6ae98d
liumengchen@ibantang.com <fbsender@lmc.local>
2017-10-27 14:16:42 +0800

    flowlover home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -56,6 +56,11 @@ class HomeController < ApplicationController
       @description = "找券网 - 找券搜券淘券抢券拿券妈妈券宝宝券猪猪券内部券一网打尽，提供淘宝优惠券、天猫优惠券、内部优惠券、内部优惠卷、隐藏优惠券、隐藏券、隐藏优惠卷等实时查询服务，先搜索领券，再淘宝下单，专享爱券网折上折。每日更新大额优惠券、品牌优惠券、一折特价底价专场、9块9包邮白菜价好东西，加入搜藏，天天省钱 - 找券网。"
       @keywords = "找券,找券网,搜券,搜券网,zhaoquan,zhaoquanwang"
     end
+    if request.host == "www.flowlover.com"
+      @title = "内部优惠券网站 - 天猫内部优惠券,天猫内部券,天猫隐藏券"
+      @description = "内部优惠券网站,提供天猫内部优惠券搜索、查询、免费领取服务，天猫内部券、隐藏券一键搜索，免费领取后可直接下单抵扣,价格超实惠，千万淘宝天猫优惠券每天更新,上淘宝(天猫)购物先上内部优惠券网站，比双十一双十二更低！"
+      @keywords = "内部优惠券,内部优惠券网站,天猫内部优惠券,天猫内部券,天猫隐藏券,天猫券"
+    end
     if request.host.include?(".youhui.iquan.net")
       brand = CompeteBrand.where(host: request.host).take
       not_found if brand.nil?
