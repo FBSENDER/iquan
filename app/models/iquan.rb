71580c82b946600385d5e72fa87fd70cce27ebc5
fbsender <lmctrello@gmail.com>
2017-12-18 17:08:01 +0800

    suggest keywords

--- app/models/iquan.rb
+++ app/models/iquan.rb
@@ -25,3 +25,7 @@ end
 class SuggestKeyword < ApplicationRecord
   self.table_name = 'iquan_suggest_keywords'
 end
+
+class CidKeywordRelation < ApplicationRecord
+  self.table_name = 'iquan_coupon_keyword_relations'
+end
