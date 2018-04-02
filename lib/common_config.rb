975e04a3533dff44e93f8386fbacf2d880723492
fbsender <lmctrello@gmail.com>
2018-04-02 10:52:41 +0800

    update 17430

---
+++ lib/common_config.rb
@@ -0,0 +1,17 @@
+$pc_host = "www.iquan.net"
+$mobile_host = "m.iquan.net"
+$heima_appkey = "124791642"
+$heima_appsecret = "159f1914339d34dc7190fde374e0d873"
+
+$taobao_app_id = "24681757"
+$taobao_app_secret = "b533b64f6cd775a62c5c6ff574f89b7a"
+$taobao_adzone_id = 146604093
+$taobao_unid = "iquanuatm"
+
+$taodianjin_pc_pid = "mm_32854514_24420321_218428309"
+$taodianjin_mobile_pid = "mm_32854514_42424136_218746221"
+
+$website_name = '爱券网'
+$big_keywords = ['内部券','内部优惠券','优惠券网站','天猫内部优惠券','淘宝内部券']
+
+$rewrite_tdk = false
