0568ec1458919c3eb8302ff2a12fc4edcea945cb
fbsender <lmctrello@gmail.com>
2018-07-12 19:20:52 +0800

    update uu logo

--- public/js/lanlan_common.js
+++ public/js/lanlan_common.js
@@ -5,7 +5,7 @@ Util.createLanlanCouponList = function(cl,obj,channel,gaPage){
         var z = cl[i];
         var re = /activityId=(\w*)/;
         //var buy_url = '/buy/' + z.itemId + '/?activity_id=' + re.exec(z.couponUrl)[1];
-        var buy_url = 'http://www.uuhaodian.com/yh/' + z.itemId + '/?from=iquan';
+        var buy_url = 'http://www.uuhaodian.com/yh/' + z.itemId + '/?from=iquan#coupon';
         var platform = '',platformPic = '';
         if(z.shopType == 'tmall')
           platform = 2;
