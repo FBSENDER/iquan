709a4de9f8332a8da46445f2c02b883c05e93623
fbsender <lmctrello@gmail.com>
2019-02-23 20:44:33 +0800

    update tdk

--- app/helpers/diyquan_helper.rb
+++ app/helpers/diyquan_helper.rb
@@ -63,7 +63,7 @@ module DiyquanHelper
   def zhekou_tdk
     @keyword = params[:keyword].force_encoding('utf-8')
     @title= "【#{@keyword}】_#{@keyword}折扣_#{@keyword}优惠券_#{$website_name}"
-    @description = "淘宝天猫隐藏#{@keyword}优惠券,#{@keyword}折扣,#{@keyowrd}包邮,20元50元100元优惠券,先领券,再下单,专享淘宝折上折优惠.领取#{@keyword}优惠券,查看#{@keyword}打折特价信息 - #{$website_name}"
+    @description = "淘宝天猫隐藏#{@keyword}优惠券,#{@keyword}折扣,#{@keyword}包邮,20元50元100元优惠券,先领券,再下单,专享淘宝折上折优惠.领取#{@keyword}优惠券,查看#{@keyword}打折特价信息 - #{$website_name}"
     @page_keywords = "#{@keyword},#{@keyword}优惠,#{@keyword}优惠券,#{@keyword}折扣,特价#{@keyword}"
     @big_keywords = $big_keywords
     @h1 = @keyword
